ac0a0d18d0142e944fb4e77473aecc6994a4cb56
kim-keypress <kim@keypress.be>
2021-01-09 17:09:49 +0100

    :tada:

---
+++ README.md
@@ -0,0 +1,9 @@
+# Chatwee PHP SDK
+## Important info
+I removed all the prefixes from the class names, so instead of ChatweeV2_Configuration you just use Configuration
+## Installation
+```
+composer require chatwee/chatwee-sdk
+```
+## Documentation
+https://chatwee.com/php-sdk
---
+++ src/Configuration.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Chatwee;
+
+class Configuration
+{
+	private static $_chatId = null;
+
+	private static $_clientKey = null;
+
+	public static function setChatId($chatId) {
+		self::$_chatId = $chatId;
+	}
+
+	public static function setClientKey($clientKey) {
+		self::$_clientKey = $clientKey;
+	}
+
+	public static function getChatId() {
+		return self::$_chatId;
+	}
+
+	public static function getClientKey() {
+		return self::$_clientKey;
+	}
+
+	public static function isConfigurationSet() {
+		return self::$_chatId !== null && self::$_clientKey !== null;
+	}
+}
