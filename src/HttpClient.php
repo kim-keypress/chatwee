300ddb50532af4cefd666d06ade92cd3f3794d36
kim-keypress <kim@keypress.be>
2021-02-06 00:44:14 +0100

    :sparkles: decode possible existing html entities before encoding them again

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -61,9 +61,8 @@ class HttpClient
 
 		$result = "";
 		foreach($parameters as $key => $value) {
-			$result .= ($key . "=" . ($value ? urlencode(htmlentities($value)) : $value) . '&');
+			$result .= ($key . "=" . ($value ? urlencode(htmlentities(htmlspecialchars_decode($value))) : $value) . '&');
 		}
-
 		$result = substr_replace($result, "", -1);
 		return $result;
 	}
